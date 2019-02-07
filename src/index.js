656f27d3cbcf4cb28c163a2f750d6248cf3ff570
vinaykhade <khadevnay.me@gmail.com>
2019-02-07 18:05:11 +0530

    created project structure

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './index.css';
+
+class ReactDataTable extends React.Component {
+    
+    render() {
+        return (
+            <div className="rc-dt-container">
+                <div className="rc-dt-table">
+                    <div className="rc-dt-body">
+                        <div className="rc-dt-row">
+                            <div className="rc-dt-column">
+                            
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ReactDataTable;
