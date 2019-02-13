926b0c012663ca2e434d704365f366cef2f075f0
vinaykhade <khadevnay.me@gmail.com>
2019-02-13 17:06:05 +0530

    added prettier configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-var path = require('path');
+const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
@@ -24,10 +24,10 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
-      },
+      }
     ]
   },
   externals: {
-    'react': 'commonjs react' 
+    react: 'commonjs react'
   }
 };
